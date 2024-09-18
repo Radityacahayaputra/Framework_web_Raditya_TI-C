9c5a7edcb35401ad5a41b1f6238525c4870303ba
Radityacahayaputra <z3kazeka717@gmail.com>
2024-09-18 02:07:26 -0700

    first commit

---
+++ application/views/Beranda/index.php
@@ -0,0 +1,95 @@
+
+ <! -- header --> 
+<div class="container">
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/Images/P0.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/Images/P1.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/Images/P2.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+</div>
+ <! -- end header -->
+<section class="about" id="about">
+ <! -- about -->
+ <div class="container">
+    <div class="row-5">
+            <div class="col-md-12 text-center">
+              <h2>ABOUT</h2>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+    <div class="col-md-5">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis dolores veniam minus!</p>
+    </div>
+    <div class="col-md-5">
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis dolores veniam minus!</p>
+    </div>
+</div>
+</div>
+</section>
+ <! -- end about -->
+ 
+<! -- kontak -->
+<section class="kontak" id="kontak"> 
+ <div class="container">
+    <div class="row mt-5">
+        <div class="col-md-12 text-center">
+        
+              <h2>KONTAK</h2>
+              <hr>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+         <div class="col-md-5">
+        <form>
+  <div class="form-group">
+    <label for="nama">Nama</label>
+    <input type="text" class="form-control" id="nama" placeholder="Masukkan Nama">
+    
+  </div>
+  <div class="form-group">
+    <label for="nama">Email</label>
+    <input type="text" class="form-control" id="email" placeholder="Masukkan Email">
+    
+  </div>
+  <div class="form-group">
+    <label for="nama">Alamat</label>
+    <input type="text" class="form-control" id="alamat" placeholder="Masukkan Alamat">
+    
+  </div>
+  <div class="form-group mb-3">
+    <label for="nama">Pesan</label>
+    <textarea type="textarea" class="form-control" id="pesan" placeholder="Masukkan Pesan"></textarea>
+    
+  </div>
+  
+  
+  <button type="submit" class="btn btn-primary">Kirim pesan</button>
+</form>
+    </div>
+ </div>
+</div>
+</div>
+</section>
+ <! -- end kontak -->
